0a5920d9b38a7d1a56d29b6f46dd8e70efd20718
simeon.yordanov <simeon.yordanov@egt-digital.com>
2026-06-06 16:17:51 +0300

    created models

---
+++ KafkaSearch-backend/KafkaSearch/KafkaSearch.Core/Models/ClusterProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace KafkaSearch.Core.Models;
+
+public class ClusterProfile
+{
+	public string ClusterName { get; set; }
+
+	public string BootstrapServerIP { get; set; }
+
+	public string KafkaClusterVersion { get; set; }
+}
---
+++ KafkaSearch-backend/KafkaSearch/KafkaSearch.Core/Models/ZookeeperSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace KafkaSearch.Core.Models;
+
+public class ZookeeperSettings
+{
+	public bool EnableZookeeperAccess { get; set; }
+
+	public string Host { get; set; }
+
+	public string Port { get; set; }
+
+	public string ChrootPath { get; set; }
+}
