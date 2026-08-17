c20772c2aee4685caee3852609c3841c1605598f
simeon.yordanov <simeon.yordanov@egt-digital.com>
2026-08-17 19:43:11 +0300

    new KafkaMessage record

---
+++ KafkaSearch-backend/KafkaSearch/KafkaSearch.Core/Models/KafkaMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace KafkaSearch.Core.Models;
+
+public sealed record KafkaMessage(
+    string Topic,
+    int Partition,
+    long Offset,
+    string? Key,
+    JsonElement Value,
+    IReadOnlyDictionary<string, byte[]> Headers,
+    DateTime Timestamp);
