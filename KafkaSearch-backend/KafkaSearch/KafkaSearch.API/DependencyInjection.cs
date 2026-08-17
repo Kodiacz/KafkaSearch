9fde000561ae33e0b78f7fbb57a252a65f373b22
simeon.yordanov <simeon.yordanov@egt-digital.com>
2026-08-17 19:41:06 +0300

    added new dependencies

--- KafkaSearch-backend/KafkaSearch/KafkaSearch.API/DependencyInjection.cs
+++ KafkaSearch-backend/KafkaSearch/KafkaSearch.API/DependencyInjection.cs
@@ -3,6 +3,7 @@
 using KafkaSearch.API.Infrastructure;
 using KafkaSearch.Core.Abstractions;
 using KafkaSearch.Core.Factories;
+using KafkaSearch.Core.Filtering;
 using KafkaSearch.Core.Services;
 using KafkaSearch.Core.Services.Interfaces;
 
@@ -12,8 +13,11 @@ public static class DependencyInjection
     {
         services.AddSingleton<IClusterProfileService, ClusterProfileService>();
         services.AddSingleton<IKafkaConnectionService, KafkaConnectionService>();
+        services.AddSingleton<IMessageScanService, MessageScanService>();
         services.AddSingleton<ITopicService, TopicService>();
         services.AddSingleton<IKafkaClientFactory, KafkaClientFactory>();
+        services.AddSingleton<IClusterClientProvider, ClusterClientProvider>();
+        services.AddSingleton<IFilterEvaluator, FilterEvaluator>();
         services.AddSingleton<IFileSystem, FileSystem>();
         return services;
     }
