e5a5a99c5bf3a0b733da52c15ce7dbef39fa8350
simeon.yordanov <simeon.yordanov@egt-digital.com>
2026-08-17 19:43:01 +0300

    added attributes to support json serialization on FilterNode and other inherited types

--- KafkaSearch-backend/KafkaSearch/KafkaSearch.Core/Filtering/FilterNode.cs
+++ KafkaSearch-backend/KafkaSearch/KafkaSearch.Core/Filtering/FilterNode.cs
@@ -1,3 +1,10 @@
-﻿namespace KafkaSearch.Core.Filtering;
+﻿using System.Text.Json.Serialization;
 
+namespace KafkaSearch.Core.Filtering;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(And), "and")]
+[JsonDerivedType(typeof(Or), "or")]
+[JsonDerivedType(typeof(Any), "any")]
+[JsonDerivedType(typeof(Compare), "compare")]
 public abstract record FilterNode;
